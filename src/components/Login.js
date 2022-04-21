db8a779d0614843ff2c359620f4e13930bf4ff17
AbielKiflu <abiel.kiflu.weld@gmail.com>
2022-04-21 14:13:47 +0200

    Modification has made

---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Login(){
+
+
+    return (
+        <>
+          <div className="login-container">
+          <form>
+                <label htmlFor="email">Email</label>
+                <input id="email" type="text" className="field" placeholder="Email"/>
+                
+                
+                <label htmlFor="pwd">Mot de passe</label>
+                <input id="pwd" type="password" className="field" placeholder="Mot de passe"/>    
+                
+
+               <input type="submit" value="Connexion" className="btn" />  <input type="submit" value="Inscription" className="btn" />
+       
+            </form>
+          </div>
+
+         </>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./index.scss";
+import Login from "./components/Login"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Login/>);
