4abcdc592e67e8384bf2d409923f5fd486ea3327
Spyros Zaboukis <spyros.zaboukis.83@gmail.com>
2022-04-21 18:56:20 +0200

    send page

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -1,16 +1,10 @@
 import React from 'react';
+import HeaderPageTitle from '../view/HeaderPageTitle';
 
 const HeaderPage = () => {
     return (
         <div>
-        <
-            <p>Je suis étrange, je suis original” <span>“Ensemble pour un accompagnement inclusif</span></p>
-            
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis fugit maiores quos consequatur 
-            optio ipsum iste explicabo odio nostrum nihil officia vitae, eum, cupiditate totam quibusdam facilis nam minima suscipit.</p>
-            
-            <p>Pôle Autisme est une plateforme à visée informative et collaborative en faveur
-                d’un accompagnement optimal de l’enfant porteur d’autisme.</p>
+            <HeaderPageTitle/>
         </div>
     );
 };
---
+++ src/view/HeaderPageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderPageTitle = () => {
+    return (
+        <div>
+            <p>Je suis étrange, je suis original” <span>“Ensemble pour un accompagnement inclusif</span></p>
+        </div>
+    );
+};
+
+export default HeaderPageTitle;
